44085fe9c4931beaa77585223a49e5488e0c651c
Alexander Lewis <alexander.lewis@ashworthhome.io>
2024-01-31 21:51:42 +1000

    Various fixes/changes

--- exporter/plex_exporter.py
+++ exporter/plex_exporter.py
@@ -28,8 +28,9 @@ class PlexExporter:
         self.server = server
         self.port = port
         try:
-            self.plex = PlexServer(baseurl=server, token=token)
+            self.plex = PlexServer(baseurl=server, token=token, timeout=5)
             self.collector = PlexCollector(self.token, self.server)
+            logging.info(f"successfully connected to {self.plex.friendlyName}")
         except Unauthorized:
             logging.error("Plex token is not valid")
             exit(1)
@@ -45,23 +46,18 @@ class PlexExporter:
         logging.info(f"serving metrics on port: {self.port}")
 
         while True:
-            self.collector.collect_users()
             self.collector.collect_base()
-            self.collector.collect_libraries()
+            self.collector.collect_libraries_genres()
             self.collector.collect_clients()
             self.collector.collect_history()
-            self.collector.collect_genres()
             self.collector.collect_qualities()
-            time.sleep(1)
+            time.sleep(15)
 
 
 class PlexCollector:
     def __init__(self, token, server) -> None:
         super().__init__()
         self.plex = PlexServer(server, token)
-        self.plex_user_metric = Gauge(
-            "plex_users", "Single user info", labelnames=["username", "id"]
-        )
         self.plex_watch_history_metric = Gauge(
             "plex_watch_history_total", "Total watch history", labelnames=["id", "user"]
         )
@@ -109,19 +105,6 @@ class PlexCollector:
             labelnames=["quality"],
         )
 
-    def collect_users(self):
-        self.plex_user_metric.clear()
-
-        try:
-            users = self.plex.systemAccounts()
-            for user in users:
-                if user.id:
-                    self.plex_user_metric.labels(id=user.id, username=user.name).set(
-                        1.0
-                    )
-        except Exception as e:
-            logging.error(e)
-
     def collect_base(self):
         self.plex_base_metric.info(
             {
@@ -135,22 +118,27 @@ class PlexCollector:
     def collect_history(self):
         users = self.plex.systemAccounts()
         history = self.plex.history()
+        try:
+            user_list = {}
+            for user in users:
+                if user.name:
+                    user_list[user.id] = user.name
 
-        user_list = {}
-        for user in users:
-            if user.name:
-                user_list[user.id] = user.name
+            history_count = []
+            for item in history:
+                user_name = user_list.get(item.accountID, "unknown")
+                history_count.append(f"{item.accountID}:{user_name}")
 
-        history_count = []
-        for item in history:
-            user_name = user_list.get(item.accountID, "unknown")
-            history_count.append(f"{item.accountID}:{user_name}")
+            final_count = Counter(history_count)
 
-        final_count = Counter(history_count)
-
-        for key, count in final_count.items():
-            user_id, user_name = key.split(":")
-            self.plex_watch_history_metric.labels(id=user_id, user=user_name).set(count)
+            for key, count in final_count.items():
+                user_id, user_name = key.split(":")
+                self.plex_watch_history_metric.labels(id=user_id, user=user_name).set(
+                    count
+                )
+        except Exception as e:
+            logging.error(e)
+            exit(1)
 
     def collect_clients(self):
         sessions = self.plex.sessions()
@@ -158,105 +146,114 @@ class PlexCollector:
 
         unique_clients = set()
 
-        for session in sessions:
-            if session.librarySectionTitle == "TV Shows":
-                title = f"{session.grandparentTitle} - {session.title}"
-            else:
-                title = session.title
+        try:
+            for session in sessions:
+                if session.librarySectionTitle == "TV shows":
+                    title = f"{session.grandparentTitle} - {session.title}"
+                else:
+                    title = session.title
 
-            session_key = str(session.sessionKey)
-            username = session.usernames[0]
-            client = session.player
+                session_key = str(session.sessionKey)
+                username = session.usernames[0]
+                client = session.player
 
-            self.plex_session_metric.labels(
-                session_key,
-                username,
-                title,
-                session.player.product,
-                session.player.state,
-                session.sessions[0].location,
-            ).set(1.0)
-
-            if client.machineIdentifier not in unique_clients:
-                self.plex_client_metric.labels(
-                    client.device, client.product, client.platform
+                self.plex_session_metric.labels(
+                    session_key,
+                    username,
+                    title,
+                    session.player.product,
+                    session.player.state,
+                    session.sessions[0].location,
                 ).set(1.0)
 
-                unique_clients.add(client.machineIdentifier)
+                if client.machineIdentifier not in unique_clients:
+                    self.plex_client_metric.labels(
+                        client.device, client.product, client.platform
+                    ).set(1.0)
+
+                    unique_clients.add(client.machineIdentifier)
+        except Exception as e:
+            logging.error(e)
+            exit(1)
 
     def collect_qualities(self):
         self.plex_movie_quality.clear()
         self.plex_show_quality.clear()
 
-        libraries = self.plex.library.sections()
-        movies = []
-        shows = []
-        for library in libraries:
-            if library.type in ["movie", "show"]:
-                for item in library.all():
-                    if item.type == "show":
-                        for episode in item.episodes():
-                            if episode.media:
-                                for media_part in episode.media:
+        try:
+            libraries = self.plex.library.sections()
+            movies = []
+            shows = []
+            for library in libraries:
+                if library.type in ["movie", "show"]:
+                    for item in library.all():
+                        if item.type == "show":
+                            for episode in item.episodes():
+                                if episode.media:
+                                    for media_part in episode.media:
+                                        video_resolution = media_part.videoResolution
+                                        shows.append(video_resolution)
+                        else:
+                            if item.media:
+                                for media_part in item.media:
                                     video_resolution = media_part.videoResolution
-                                    shows.append(video_resolution)
-                    else:
-                        if item.media:
-                            for media_part in item.media:
-                                video_resolution = media_part.videoResolution
-                                video_resolution = media_part.videoResolution
-                                movies.append(video_resolution)
+                                    video_resolution = media_part.videoResolution
+                                    movies.append(video_resolution)
 
-        count_shows = Counter(shows)
-        count_movies = Counter(movies)
-        shows = dict(count_shows)
-        movies = dict(count_movies)
+            count_shows = Counter(shows)
+            count_movies = Counter(movies)
+            shows = dict(count_shows)
+            movies = dict(count_movies)
 
-        for quality, count in movies.items():
-            self.plex_movie_quality.labels(quality).set(count)
-        for quality, count in shows.items():
-            self.plex_show_quality.labels(quality).set(count)
+            for quality, count in movies.items():
+                self.plex_movie_quality.labels(quality).set(count)
+            for quality, count in shows.items():
+                self.plex_show_quality.labels(quality).set(count)
+        except Exception as e:
+            logging.error(e)
+            exit(1)
 
-    def collect_genres(self):
+    def collect_libraries_genres(self):
         libraries = self.plex.library.sections()
 
-        genres_lists = []
-        for section in libraries:
-            if section.TYPE == "show" or "movie":
-                media = section.all()
-                for media in media:
-                    for genre in media.genres:
-                        genres_lists.append(genre.tag)
-        genre_count = Counter(genres_lists)
-        genres = dict(genre_count)
+        try:
+            genres_lists = []
+            for section in libraries:
+                if section.TYPE == "show" or "movie":
+                    media = section.all()
+                    for media in media:
+                        for genre in media.genres:
+                            genres_lists.append(genre.tag)
+            genre_count = Counter(genres_lists)
+            genres = dict(genre_count)
 
-        for genre, count in genres.items():
-            self.plex_genres_metric.labels(genre, self.plex.friendlyName).set(count)
+            for genre, count in genres.items():
+                self.plex_genres_metric.labels(genre, self.plex.friendlyName).set(count)
 
-    def collect_libraries(self):
-        libraries = self.plex.library.sections()
+            for section in libraries:
+                if section.TYPE == "show":
+                    episodes_total_size = len(section.searchEpisodes())
 
-        for section in libraries:
-            if section.TYPE == "show":
-                episodes_total_size = len(section.searchEpisodes())
+                    self.plex_library_size_metric.labels(
+                        section.title, self.plex.friendlyName, section.type
+                    ).set(section.totalStorage)
 
-                self.plex_library_size_metric.labels(
-                    section.title, self.plex.friendlyName, section.type
-                ).set(section.totalStorage)
+                    self.plex_library_items_metric.labels(
+                        section.title, self.plex.friendlyName, section.type
+                    ).set(section.totalSize)
 
-                self.plex_library_items_metric.labels(
-                    section.title, self.plex.friendlyName, section.type
-                ).set(section.totalSize)
+                    self.plex_library_items_metric.labels(
+                        "Episodes", self.plex.friendlyName, section.type
+                    ).set(episodes_total_size)
 
-                self.plex_library_items_metric.labels(
-                    "Episodes", self.plex.friendlyName, section.type
-                ).set(episodes_total_size)
+                else:
+                    self.plex_library_size_metric.labels(
+                        section.title, self.plex.friendlyName, section.type
+                    ).set(section.totalStorage)
 
-            else:
-                self.plex_library_size_metric.labels(
-                    section.title, self.plex.friendlyName, section.type
-                ).set(section.totalStorage)
-
-                self.plex_library_items_metric.labels(
-                    section.title, self.plex.friendlyName, section.type
-                ).set(section.totalSize)
+                    self.plex_library_items_metric.labels(
+                        section.title, self.plex.friendlyName, section.type
+                    ).set(section.totalSize)
+        except Exception as e:
+            logging.error(e)
+            exit(1)
